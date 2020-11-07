29c3bfc0f6515fa2f2a0400aa87d4598a0fa46b6
David <davidjohansenakv@gmail.com>
2020-11-07 15:29:33 +0100

    Let's see if this works

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Values = new string[2] { "2", "3" };
+            Prompt("Hey Jude!");
+
+        }
+
+        static void Prompt(string msg)
+        { 
+            Console.WriteLine($"=> {msg}" ); 
+        }
+    }   
+}
